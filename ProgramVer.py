00e8f26ad9b15b090c02a06b95c048f8218ac645
William <willtheorangeguy@users.noreply.github.com>
2020-01-17 15:11:48 -0800

    Add an actual working License and EULA text window
    Also... update the copyright year

--- ProgramVer.py
+++ ProgramVer.py
@@ -1,5 +1,5 @@
-# This is the standard program used by Dog Face Development Co. for versioning their products.
-# Copyright (C) 2017 - 2019 Dog Face Development Co.
+# This is the standard program used by Dog Face Development Co. for versioning and displaying copyright infor for their products.
+# Copyright (C) 2017 - 2020 Dog Face Development Co.
 
 # Import Statements
 from tkinter import *
@@ -7,10 +7,24 @@ import os
 
 # Document Functions
 def openLicense():
-    os.startfile("LICENSE.txt") #change file address as needed
+    windowl = Tk()
+    licensefile = open('LICENSE.txt', 'r') #change file address as needed
+    licensecontents = licensefile.read()
+    licensefile.close()
+    windowl.title('License')
+    licensetext = Text(windowl)
+    licensetext.insert(INSERT, licensecontents)
+    licensetext.pack()
 
 def openEULA():
-    os.startfile("EULA.txt") #change file address as needed
+    windowl = Tk()
+    eulafile = open('EULA.txt', 'r')  # change file address as needed
+    eulacontents = eulafile.read()
+    eulafile.close()
+    windowl.title('EULA')
+    eulatext = Text(windowl)
+    eulatext.insert(INSERT, eulacontents)
+    eulatext.pack()
 
 # ProgramVer Function
 def ProgramVer():
@@ -23,9 +37,9 @@ def ProgramVer():
     dfdlogo = Label(window, image = dfdimage)
     pythonpowered = Label(window, image = pythonimage)
     info = Label(window, text='ProgramVer \n Version: 1.8.0 (Build 1080)') #change respectively
-    trademarks = Label(window, text='Copyright (C) 2017 - 2019 Dog Face Development Co. All rights reserved in all countries. \n ProgramVer and its code, user interface and all other associated trademarks are protected \nby trademarks and copyright in Canada, the United States and other countries.') #change as needed
+    trademarks = Label(window, text='Copyright (C) 2017 - 2020 Dog Face Development Co. All rights reserved in all countries. \n ProgramVer and its code, user interface and all other associated trademarks are protected \nby trademarks and copyright in Canada, the United States and other countries.') #change as needed
     licenseblurb = Label(window, text="""\n ProgramVer - Version window for DFD Co.'s programs
-        Copyright (C) 2017-2019 Dog Face Development Company
+        Copyright (C) 2017-2020 Dog Face Development Company
     
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
