d5ef8e873d5c7b4f5a5bed369c2b6a2da297785f
Restyled.io <commits@restyled.io>
2023-01-03 12:41:48 +0000

    Restyled by isort

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+import os
 # Import Statements
 from tkinter import *
-import os
 
 # Document Functions
 
