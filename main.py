6bcdc5371217d02427bab4fd004972f360025544
William <willtheorangeguy@users.noreply.github.com>
2024-01-01 16:58:26 -0700

    Bump copyright year to 2024

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 ProgramVer - A Python version of Microsoft's 'winver'.
-Copyright (C) 2017-2023 Dog Face Development Co.
+Copyright (C) 2017-2024 Dog Face Development Co.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -65,7 +65,7 @@ def ProgramVer():
     )  # change respectively
     trademarks = Label(
         window,
-        text="Copyright (C) 2017 - 2023 Dog Face Development Co. \
+        text="Copyright (C) 2017 - 2024 Dog Face Development Co. \
             All rights reserved in all countries. \
                 \n ProgramVer and its code, user interface and all other associated trademarks are protected \
                 \nby trademarks and copyright in Canada, the United States and other countries.",
@@ -73,7 +73,7 @@ def ProgramVer():
     licenseblurb = Label(
         window,
         text="""\n ProgramVer - Version window for DFD Co.'s programs
-        Copyright (C) 2017-2023 Dog Face Development Company
+        Copyright (C) 2017-2024 Dog Face Development Company
 
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
